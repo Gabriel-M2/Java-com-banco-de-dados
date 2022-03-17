0e8258aeb6e4c311340b3b72b2344b90cb0c41d9
Gabriel <gmontenegro1227@gmail.com>
2022-03-17 18:40:23 -0300

    Primeiro Commit| Aula 1

---
+++ src/conection/CarroJ.java
@@ -0,0 +1,33 @@
+package conection;
+
+public class CarroJ {
+    
+    private String placa;
+    private String cor;
+    private String desc;
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
